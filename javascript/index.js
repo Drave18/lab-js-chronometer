be8697e7bd02dedc83c7864e715a00b5c0b0108d
David Mejia <davidsebas1825@gmail.com>
2025-06-10 17:11:33 -0500

    Done

--- javascript/index.js
+++ javascript/index.js
@@ -9,57 +9,100 @@ const minDecElement = document.getElementById('minDec');
 const minUniElement = document.getElementById('minUni');
 const secDecElement = document.getElementById('secDec');
 const secUniElement = document.getElementById('secUni');
-const milDecElement = document.getElementById('milDec');
-const milUniElement = document.getElementById('milUni');
+
 const splitsElement = document.getElementById('splits');
 
 function printTime() {
-  // ... your code goes here
+  printMinutes()
+  printSeconds()
 }
 
+//Print minuts & seconds
 function printMinutes() {
-  // ... your code goes here
+
+  const minutes = chronometer.getMinutes()
+  const minutesValue = chronometer.computeTwoDigitNumber(minutes)
+
+  minDecElement.innerHTML = minutesValue[0]
+  minUniElement.innerHTML = minutesValue[1];
 }
 
 function printSeconds() {
-  // ... your code goes here
+
+  const seconds = chronometer.getSeconds();
+  const secondsValue = chronometer.computeTwoDigitNumber(seconds);
+
+  secDecElement.innerHTML = secondsValue[0];
+  secUniElement.innerHTML = secondsValue[1];
 }
 
-// ==> BONUS
-function printMilliseconds() {
-  // ... your code goes here
-}
-
+//Display splits by creatin a li element with the value of split()
 function printSplit() {
-  // ... your code goes here
+  //3. insert the li split inside the ol
+
+  const split = document.createElement('li')
+  split.innerText = chronometer.split()
+
+  splitsElement.appendChild(split)
+
+
 }
 
+//Clear the ol element that store splits
 function clearSplits() {
-  // ... your code goes here
-}
+  splitsElement.innerHTML = '';
 
-function setStopBtn() {
-  // ... your code goes here
-}
-
-function setSplitBtn() {
-  // ... your code goes here
 }
 
 function setStartBtn() {
-  // ... your code goes here
+  btnLeftElement.className = "btn stop"
+  btnLeftElement.innerText = "STOP"
+  btnRightElement.className = "btn split"
+  btnRightElement.innerText="SPLIT"
 }
 
-function setResetBtn() {
-  // ... your code goes here
+function setStopBtn() {
+  btnLeftElement.className = 'btn start';
+  btnLeftElement.innerText = 'START';
+  btnRightElement.className = 'btn reset';
+  btnRightElement.innerText = 'RESET';
 }
+
+
+function setResetBtn() {
+  clearSplits()
+  const spans = document.querySelectorAll('#sphere span:not(#dots)');
+  spans.forEach((span) => {
+    span.innerText = '0';
+  });
+}
+
+function setSplitBtn() {
+  printSplit()
+}
+
+
 
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
-  // ... your code goes here
+  if(btnLeftElement.classList.contains('start')){
+    setStartBtn()
+    chronometer.start(printTime);
+  }
+  else if (btnLeftElement.classList.contains('stop')){
+    setStopBtn()
+    chronometer.stop(printTime);
+  }
 });
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
-  // ... your code goes here
+  if(btnRightElement.classList.contains('reset')){
+    setResetBtn()
+    chronometer.reset();
+  }
+  else if(btnRightElement.classList.contains('split')){
+    setSplitBtn()
+  }
 });
+
